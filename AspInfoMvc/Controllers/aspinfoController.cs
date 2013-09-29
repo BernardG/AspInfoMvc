bec0d614e499f79b96dd72a046cb00aea68f5760
Bernard Grosperrin <bgrosperrin@laposte.net>
2013-09-29 17:49:52 +0200

    First commit after checking it worked on Ubuntu/Mono
    Pretty much complete version of this little tool to check if MVC is
    working OK. Mostly  made to check Mono compatibility.

---
+++ AspInfoMvc/Controllers/aspinfoController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspInfoMvc.Controllers
+{
+    public class InfoClass
+    {
+        public string OperatingSystem
+        {
+            get { return Environment.OSVersion.ToString(); }
+        }
+
+        public string[] ServerSoftware
+        {
+            get { return HttpContext.Current.Request.ServerVariables.GetValues("SERVER_SOFTWARE"); }
+        }
+
+        public System.Reflection.Assembly[] LoadedAssemblies
+        {
+            get { return AppDomain.CurrentDomain.GetAssemblies().Concat(AppDomain.CurrentDomain.GetAssemblies()).Distinct().OrderBy(o => o.FullName).ToArray(); }
+        }
+
+        public System.Reflection.Assembly MvcDll
+        {
+            get { return AppDomain.CurrentDomain.Load("System.Web.Mvc"); }
+        }
+
+        public NameValueCollection ServerVariables
+        {
+            get { return HttpContext.Current.Request.ServerVariables; }
+        }
+
+        public string NetVersion
+        {
+            get { return Environment.Version.ToString(); }
+        }
+
+        public int NetBitness
+        {
+            get { return IntPtr.Size * 8; }
+        }               
+    }
+
+    public class AspInfoController : Controller
+    {
+        //
+        // GET: /AspInfo/
+        public InfoClass MyInfo = new InfoClass();
+
+        public ActionResult Index()
+        {
+
+            return View(MyInfo);
+        }
+
+    }
+}
+
+
